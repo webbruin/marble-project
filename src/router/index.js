5779bbf17b258fb7968c7715a4eb23865141b3f8
andy.xiong <andy.xiong@intramirror.com>
2026-04-07 20:48:24 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Main from '../views/Main.vue';
+import Home from '../views/Home.vue';
+import Shop from '../views/Shop.vue';
+import Cart from '../views/Cart.vue';
+import My from '../views/My.vue';
+import Login from '../views/Login.vue';
+import SetPassword from '../views/SetPassword.vue';
+import ChangePassword from '../views/ChangePassword.vue';
+import ForgetPassword from '../views/ForgetPassword.vue';
+
+import Room from '../views/Room.vue';
+import CheckIn from '../views/CheckIn.vue';
+import Ranking from '../views/Ranking.vue';
+import Setting from '../views/Setting.vue';
+import AccountCancel from '../views/AccountCancel.vue';
+import EditUserInfo from '../views/EditUserInfo.vue';
+import Aggrement1 from '../views/Aggrement/Aggrement1.vue';
+import Bluetooth from '../views/Bluetooth.vue';
+
+import NotFountView from '../views/404.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/login', name: 'login', component: import('../views/Login.vue') },
-    { path: '/set-password', name: 'set-password', component: import('../views/SetPassword.vue') },
-    { path: '/change-password', name: 'change-password', component: import('../views/ChangePassword.vue') },
-    { path: '/forget-password', name: 'forget-password', component: import('../views/ForgetPassword.vue') },
     {
       path: '/',
       name: 'main',
-      component: import('../views/Main.vue'),
+      component: Main,
       children: [
-        { path: '/', name: 'home', component: import('../views/Home.vue') },
-        { path: '/shop', name: 'shop', component: import('../views/Shop.vue') },
-        { path: '/cart', name: 'cart', component: import('../views/Cart.vue') },
-        { path: '/my', name: 'my', component: import('../views/My.vue') },
+        { path: '/', name: 'home', component: Home },
+        { path: '/shop', name: 'shop', component: Shop },
+        { path: '/cart', name: 'cart', component: Cart },
+        { path: '/my', name: 'my', component: My },
       ]
     },
-    { path: '/room/:id', name: 'room', component: import('../views/Room.vue') },
-    { path: '/check-in', name: 'check-in', component: import('../views/CheckIn.vue') },
-    { path: '/ranking', name: 'ranking', component: import('../views/Ranking.vue') },
-    { path: '/setting', name: 'setting', component: import('../views/Setting.vue') },
-    { path: '/account-cancel', name: 'account-cancel', component: import('../views/AccountCancel.vue') },
-    { path: '/edit-user-info', name: 'edit-user-info', component: import('../views/EditUserInfo.vue') },
-    { path: '/aggrement1', name: 'aggrement1', component: import('../views/Aggrement/Aggrement1.vue') },
-    { path: '/bluetooth', name: 'bluetooth', component: import('../views/Bluetooth.vue') },
+    { path: '/login', name: 'login', component: Login },
+    { path: '/set-password', name: 'set-password', component: SetPassword },
+    { path: '/change-password', name: 'change-password', component: ChangePassword },
+    { path: '/forget-password', name: 'forget-password', component: ForgetPassword },
+    { path: '/room/:id', name: 'room', component: Room },
+    { path: '/check-in', name: 'check-in', component: CheckIn },
+    { path: '/ranking', name: 'ranking', component: Ranking },
+    { path: '/setting', name: 'setting', component: Setting },
+    { path: '/account-cancel', name: 'account-cancel', component: AccountCancel },
+    { path: '/edit-user-info', name: 'edit-user-info', component: EditUserInfo },
+    { path: '/aggrement1', name: 'aggrement1', component: Aggrement1 },
+    { path: '/bluetooth', name: 'bluetooth', component: Bluetooth },
     // 404
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: import('../views/404.vue') },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFountView },
   ],
 })
 
